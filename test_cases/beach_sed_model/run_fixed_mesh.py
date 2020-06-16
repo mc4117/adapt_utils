2055da371d48b3907675aa5c40a270fec1689fef
mc4117 <mc4117@imperial.ac.uk>
2020-06-16 14:50:10 +0100

    fixed mesh

--- test_cases/beach_sed_model/run_fixed_mesh.py
+++ test_cases/beach_sed_model/run_fixed_mesh.py
@@ -11,7 +11,9 @@ from adapt_utils.swe.morphological.solver import UnsteadyShallowWaterProblem
 
 t1 = time.time()
 
-nx = 1.5
+t1 = time.time()
+
+nx = 4.0
 
 ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
@@ -25,8 +27,8 @@ op = BeachOptions(approach='fixed_mesh',
                    num_adapt=1,
                    friction='manning',
                    nx=nx,
-                   ny=1,
-                   input_dir = 'hydrodynamics_beach_l_sep_nx_330.0',
+                   ny=2,
+                   input_dir = 'hydrodynamics_beach_l_sep_nx_880',
                    output_dir = outputdir,
                    r_adapt_rtol=1.0e-3,
                    init = True)
