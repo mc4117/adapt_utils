4654c0555d5dc6ec2b349ae61d6c46e7b5fec7bf
mc4117 <mc4117@imperial.ac.uk>
2020-06-28 14:27:02 +0100

    trench slant

--- test_cases/trench_slant/post_processing_fixed.py
+++ test_cases/trench_slant/post_processing_fixed.py
@@ -29,12 +29,14 @@ bath1 = initialise_fields(new_mesh, 'hydrodynamics_trench_slant_bath_0.1')
 bath2 = initialise_fields(new_mesh, 'hydrodynamics_trench_slant_bath_0.2')
 bath3 = initialise_fields(new_mesh, 'hydrodynamics_trench_slant_bath_0.4')
 bath4 = initialise_fields(new_mesh, 'hydrodynamics_trench_slant_bath_0.8')
-bath_real =  initialise_fields(new_mesh, 'hydrodynamics_trench_slant_bath_2.0')
+bath5 =  initialise_fields(new_mesh, 'hydrodynamics_trench_slant_bath_2.0')
+bath_real = initialise_fields(new_mesh, 'hydrodynamics_trench_slant_bath_4.0')
 
 errorlist = []
 errorlist.append(fire.errornorm(bath1, bath_real))
 errorlist.append(fire.errornorm(bath2, bath_real))
 errorlist.append(fire.errornorm(bath3, bath_real))
 errorlist.append(fire.errornorm(bath4, bath_real))
+errorlist.append(fire.errornorm(bath5, bath_real))
 
 print(errorlist)
